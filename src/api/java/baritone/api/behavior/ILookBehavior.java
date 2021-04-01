a8d793f9f68be8501fa07d9bcde705af7e9e9689
Pyrofab <redstoneinfire@gmail.com>
2021-04-01 19:42:26 +0200

    Decouple basic movements from look direction

--- src/api/java/baritone/api/behavior/ILookBehavior.java
+++ src/api/java/baritone/api/behavior/ILookBehavior.java
@@ -17,6 +17,8 @@
 
 package baritone.api.behavior;
 
+import baritone.api.Settings;
+import baritone.api.utils.IInputOverrideHandler;
 import baritone.api.utils.Rotation;
 
 /**
@@ -32,8 +34,42 @@ public interface ILookBehavior extends IBehavior {
      * If any sort of block interaction is required, force should be {@code true},
      * otherwise, it should be {@code false};
      *
+     * <p>This method should be called exactly once a tick while a process is ongoing.
+     * If it is not called in a tick, the entity's rotations will not be touched even
+     * if they do not match the previous target anymore. If it is called more than once in a tick,
+     * only the last call will be taken into account.
+     *
      * @param rotation The target rotations
      * @param force    Whether or not to "force" the rotations
      */
     void updateTarget(Rotation rotation, boolean force);
+
+    /**
+     * Updates the current {@link ILookBehavior} secondary target to target
+     * the specified rotations on the next tick.
+     *
+     * <p>This target will be handled differently based on whether {@link #updateTarget(Rotation, boolean)}
+     * has been called in the same tick:
+     * <ul>
+     *     <li>If no primary target has been set, the secondary target will be treated as a primary target.</li>
+     *     <li>If a primary target has been set with {@code force = false}, the entity's rotation will
+     *     be updated to match the <em>secondary</em> target, but {@linkplain IInputOverrideHandler inputs}
+     *     will be updated such that the direction of the movement is the same as if the primary target had been used.</li>
+     *     <li>If a primary target has been set with {@code force = true}, the secondary target is discarded.</li>
+     * </ul>
+     *
+     * <p><strong>The secondary target will be ignored if sprinting has been requested for this tick.</strong>
+     * This is because we can only sprint forward, and arbitrarily cancelling sprints would mess with cost estimates,
+     * parkour moves, and other processes. If you want to ensure more reliable secondary target following, set
+     * {@link Settings#allowSprint} to {@code false}.
+     *
+     * <p>This method should be called at most once a tick.
+     * If it is not called in a tick, this {@link ILookBehavior} will use only the primary target when
+     * updating rotations. If it is called more than once in a tick, only the last call will be taken into account.
+     *
+     * @param target the secondary target rotations
+     * @apiNote this method can be used to look in the direction of an entity while moving, e.g. to simulate aiming
+     * or to block incoming attacks.
+     */
+    void updateSecondaryTarget(Rotation target);
 }
--- src/main/java/baritone/behavior/LookBehavior.java
+++ src/main/java/baritone/behavior/LookBehavior.java
@@ -18,9 +18,15 @@
 package baritone.behavior;
 
 import baritone.Baritone;
-import baritone.api.Settings;
 import baritone.api.behavior.ILookBehavior;
 import baritone.api.utils.Rotation;
+import baritone.api.utils.input.Input;
+import baritone.utils.InputOverrideHandler;
+import com.google.common.base.Preconditions;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.MathHelper;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.Nullable;
 
 public final class LookBehavior extends Behavior implements ILookBehavior {
 
@@ -28,21 +34,20 @@ public final class LookBehavior extends Behavior implements ILookBehavior {
      * Target's values are as follows:
      */
     private Rotation target;
+    private Rotation secondaryTarget;
 
     /**
      * Whether or not rotations are currently being forced
      */
     private boolean force;
 
-    /**
-     * The last player yaw angle. Used when free looking
-     *
-     * @see Settings#freeLook
-     */
-    private float lastYaw;
-
     public LookBehavior(Baritone baritone) {
         super(baritone);
+    }
+
+    @Override
+    public void updateSecondaryTarget(Rotation target) {
+        this.secondaryTarget = target;
     }
 
     @Override
@@ -55,43 +60,117 @@ public final class LookBehavior extends Behavior implements ILookBehavior {
             }
             this.target = new Rotation(this.target.getYaw() + (float) (rand * baritone.settings().randomLooking113.get()), this.target.getPitch());
         }
-        this.force = force || !baritone.settings().freeLook.get();
+        this.force = force;
     }
 
     @Override
     public void onTickServer() {
-        if (this.target == null) {
-            return;
+        if (this.target != null || this.secondaryTarget != null) {
+            this.updateLook(this.target, this.force, this.secondaryTarget);
         }
 
-        if (this.force) {
-            ctx.entity().yaw = this.target.getYaw();
-            float oldPitch = ctx.entity().pitch;
-            float desiredPitch = this.target.getPitch();
-            ctx.entity().pitch = desiredPitch;
-            ctx.entity().yaw += (Math.random() - 0.5) * baritone.settings().randomLooking.get();
-            ctx.entity().pitch += (Math.random() - 0.5) * baritone.settings().randomLooking.get();
-            if (desiredPitch == oldPitch && !baritone.settings().freeLook.get()) {
-                nudgeToLevel();
-            }
-            this.target = null;
+        this.target = null;
+        this.secondaryTarget = null;
+        this.force = false;
+    }
+
+    @Contract("null, true, _ -> fail; null, _, null -> fail")
+    private void updateLook(@Nullable Rotation primaryTarget, boolean forcePrimary, @Nullable Rotation secondaryTarget) {
+        Preconditions.checkArgument(primaryTarget != null || !forcePrimary);
+        Preconditions.checkArgument(primaryTarget != null || secondaryTarget != null);
+
+        Rotation actualTarget;
+
+        if (!forcePrimary && !this.baritone.getInputOverrideHandler().isInputForcedDown(Input.SPRINT)) {
+            // If we are sprinting, we really need to look in the right direction, otherwise we will
+            // mess up the current path and possibly a bunch of other things
+            actualTarget = getActualTarget(primaryTarget, secondaryTarget);
+            if (actualTarget == null) return;
+        } else {
+            actualTarget = primaryTarget;
         }
+
+        assert actualTarget != null;
+        LivingEntity entity = this.ctx.entity();
+        double lookScrambleFactor = baritone.settings().randomLooking.get();
+        updateLook(entity, actualTarget, lookScrambleFactor, !baritone.settings().freeLook.get());
+    }
+
+    private static void updateLook(LivingEntity entity, Rotation target, double lookScrambleFactor, boolean nudgePitch) {
+        entity.yaw = target.getYaw();
+        float oldPitch = entity.pitch;
+        float desiredPitch = target.getPitch();
+        entity.pitch = desiredPitch;
+        entity.yaw += (Math.random() - 0.5) * lookScrambleFactor;
+        entity.pitch += (Math.random() - 0.5) * lookScrambleFactor;
+        if (desiredPitch == oldPitch && nudgePitch) {
+            nudgeToLevel(entity);
+        }
+    }
+
+    @Nullable
+    private Rotation getActualTarget(@Nullable Rotation primaryTarget, @Nullable Rotation secondaryTarget) {
+        if (baritone.settings().freeLook.get()) {
+            // free look is enabled, do not touch the rotations but make sure we move correctly
+            updateControlsToMatch(this.baritone.getInputOverrideHandler(), primaryTarget, this.ctx.entity().yaw);
+            return null;
+        } else if (secondaryTarget != null) {
+            // we have a secondary target, use it to set the rotations but still make sure we move correctly
+            updateControlsToMatch(this.baritone.getInputOverrideHandler(), primaryTarget, secondaryTarget.getYaw());
+            return secondaryTarget;
+        }
+        // We could look elsewhere, but we have nowhere more important to look at than where we are going
+        return primaryTarget;
     }
 
     public void pig() {
         if (this.target != null) {
-            ctx.entity().yaw = this.target.getYaw();
+            this.ctx.entity().yaw = this.target.getYaw();
+        }
+    }
+
+    private static void updateControlsToMatch(InputOverrideHandler inputs, Rotation target, float actualYaw) {
+        if (target == null) return;
+        // TODO handle other directional movement keys being pressed
+        // no hurry though, no process uses them currently
+        if (!inputs.isInputForcedDown(Input.MOVE_FORWARD)) return;
+
+        float desiredYaw = MathHelper.wrapDegrees(target.getYaw());
+        float yawDifference = MathHelper.subtractAngles(actualYaw, desiredYaw);
+        // +/-0 -> looking where we should
+        // -90 -> looking to the right of where we should -> need to move to the left
+        // +90 -> looking to the left of where we should -> need to move to the right
+        // +/-180 -> looking backwards
+        float absoluteDifference = Math.abs(yawDifference);
+
+        if (absoluteDifference >= 89) {
+            // not going forward at all
+            inputs.setInputForceState(Input.MOVE_FORWARD, false);
+
+            if (absoluteDifference >= 91) {
+                // actually going backwards
+                inputs.setInputForceState(Input.MOVE_BACK, true);
+            }
+        }
+
+        if (absoluteDifference >= 1 && absoluteDifference <= 179) {
+            // going diagonal or sideways
+            if (yawDifference > 0) {
+                inputs.setInputForceState(Input.MOVE_RIGHT, true);
+            } else {
+                inputs.setInputForceState(Input.MOVE_LEFT, true);
+            }
         }
     }
 
     /**
      * Nudges the player's pitch to a regular level. (Between {@code -20} and {@code 10}, increments are by {@code 1})
      */
-    private void nudgeToLevel() {
-        if (ctx.entity().pitch < -20) {
-            ctx.entity().pitch++;
-        } else if (ctx.entity().pitch > 10) {
-            ctx.entity().pitch--;
+    private static void nudgeToLevel(LivingEntity entity) {
+        if (entity.pitch < -20) {
+            entity.pitch++;
+        } else if (entity.pitch > 10) {
+            entity.pitch--;
         }
     }
 }
